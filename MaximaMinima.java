25c467dfa350df53950d7354cf73f71e70fc7325
satvikDesktop <satviksingh2304@gmail.com>
2021-08-20 19:27:28 +0530

    function for minimum maximum in list

---
+++ MaximaMinima.java
@@ -0,0 +1,59 @@
+import java.util.Scanner;
+public class MaximaMinima {
+
+    static Scanner sc = new Scanner(System.in);
+    static int count = sc.nextInt();
+    static int arr [] = new int [20] ;
+    
+      
+     public static int max(){
+         int temp ;
+        for (int i = 0; i < count; i++)   
+        {  
+            for (int j = i + 1; j < count; j++)   
+            {  
+                if (arr[i] > arr[j])   
+                {  
+                    temp = arr[i];  
+                    arr[i] = arr[j];  
+                    arr[j] = temp;  
+                }  
+            }  
+        }  
+       return arr[count-1];  
+}  
+
+  
+
+    public static int min() {
+        int temp ;
+        for (int i = 0; i < count; i++)   
+        {  
+            for (int j = i + 1; j < count; j++)   
+            {  
+                if (arr[i] > arr[j])   
+                {  
+                    temp = arr[i];  
+                    arr[i] = arr[j];  
+                    arr[j] = temp;  
+                }  
+            }  
+        }  
+       return arr[0]; 
+    }
+
+
+
+    public static void main (String[]args) {
+        System.out.println("ENTER ARRAY ELEMENTS ");
+        for (int i=0;i<count;i++) {
+            arr[i]=sc.nextInt();
+        }
+      
+        System.out.println("MAXIMUM : "+max()); 
+        System.out.println("MINIMUM : "+min());
+          
+       }
+
+    }
+
